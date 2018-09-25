8f36f104f0c15f09e694c82debf2cf88f52794fd
Krzysztof Szypulski <krzychu700@gmail.com>
2018-09-25 21:32:01 +0200

    fixed error in app.js

--- components/App.js
+++ components/App.js
@@ -1,47 +1,48 @@
 var GIPHY_API_URL = 'https://api.giphy.com';
-var GIPHY_PUB_KEY = 'eByp0BaZfZZxgnEQlWCqD0J5LEqJYKyL';
+var GIPHY_PUB_KEY = '4sTdgv6tRulC0aYMk1isg99GsLo0HSJs';
 
 App = React.createClass({
-    
-        getInitialState() {
-            return {
-                loading: false,
-                searchingText: '',
-                gif: {}
-            };
-        },
 
-        handleSearch: function(searchingText) {  // 1.
-            this.setState({
-              loading: true  // 2.
-            });
-            this.getGif(searchingText, function(gif) {  // 3.
-              this.setState({  // 4
-                loading: false,  // a
-                gif: gif,  // b
-                searchingText: searchingText  // c
-              });
-            }.bind(this));
-          },
+    getInitialState() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
 
-          getGif: function(searchingText, callback) {  // 1.
-            var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-            var xhr = new XMLHttpRequest();  // 3.
-            xhr.open('GET', url);
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                   var data = JSON.parse(xhr.responseText).data; // 4.
-                    var gif = {  // 5.
-                        url: data.fixed_width_downsampled_url,
-                        sourceUrl: data.url
-                    };
-                    callback(gif);  // 6.
-                }
-            };
-            xhr.send();
-        },
+    handleSearch: function(searchingText) {  // 1.
+        this.setState({
+          loading: true  // 2.
+        });
+        this.getGif(searchingText, function(gif) {  // 3.
+          this.setState({  // 4
+            loading: false,  // a
+            gif: gif,  // b
+            searchingText: searchingText  // c
+          });
+        }.bind(this));
+    },
 
-        render: function() {
+    getGif: function(searchingText, callback) {  // 1.
+        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+        var xhr = new XMLHttpRequest();  // 3.
+        xhr.open('GET', url);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+               var data = JSON.parse(xhr.responseText).data; // 4.
+                var gif = {  // 5.
+                    url: data.fixed_width_downsampled_url,
+                    sourceUrl: data.url
+                };
+                callback(gif);  // 6.
+            }
+        };
+        xhr.send();
+    },
+
+    render: function() {
+
         var styles = {
             margin: '0 auto',
             textAlign: 'center',
@@ -50,14 +51,14 @@ App = React.createClass({
 
         return (
           <div style={styles}>
-                <h1>Wyszukiwarka GIFow!</h1>
+                <h1>Wyszukiwarka GIFów!</h1>
                 <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
                 <Search onSearch={this.handleSearch}/>
-                <Gif
-                    loading={this.state.loading}
-                    url={this.state.gif.url}
-                    sourceUrl={this.state.gif.sourceUrl}
-                />
+            <Gif
+                loading={this.state.loading}
+                url={this.state.gif.url}
+                sourceUrl={this.state.gif.sourceUrl}
+            />
           </div>
         );
     }
